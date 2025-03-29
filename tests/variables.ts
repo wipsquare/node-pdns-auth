02b25de8474ae9ce4f8c45f0fb090742ab34e58a
Ovidiu Bratosin <ovidiu@bratosin.net>
2025-03-29 14:35:36 +0100

    ci: test workflow

--- tests/variables.ts
+++ tests/variables.ts
@@ -1,11 +1,11 @@
 import {createPowerDNSAdvancedClient, createPowerDNSSimpleClient} from "../src/lib/powerdns";
 
 export const SIMPLE_CLIENT = createPowerDNSSimpleClient({
-    baseUrl: 'http://pdns-auth:8081/api/v1',
+    baseUrl: 'http://localhost:8081/api/v1',
     apiKey: 'apikey'
 });
 
 export const ADVANCED_CLIENT = createPowerDNSAdvancedClient({
-    baseUrl: 'http://pdns-auth:8081/api/v1',
+    baseUrl: 'http://localhost:8081/api/v1',
     apiKey: 'apikey'
 });
